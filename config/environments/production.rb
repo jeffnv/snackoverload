0f6ffb0b556e1dbe89b7bf116b5c649c927b11ec
Jeffrey Fiddler <jeffnv@gmail.com>
2013-11-05 13:48:40 -0800

    Devise login working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,9 @@ Snackoverload::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  config.action_mailer.default_url_options = { 
+    :host => 'http://snackoverload.com' 
+  }
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Snackoverload::Application.routes.draw do
+  devise_for :users
+
   root to: 'questions#index'
   resources :questions, only:[:index]
 
